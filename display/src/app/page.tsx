b80e075e5852970ea199440fb4f4eecca701d33d
Francisco-SP3 <a01177893@tec.mx>
2023-10-03 16:24:19 -0600

    Change logo to main page

--- display/src/app/page.tsx
+++ display/src/app/page.tsx
@@ -2,10 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Hola Paco
-      </div>
-    </main>
+    <div 
+         className="bg-black ...">
+            <Image 
+             src="/images/roborregos_logo.png"
+             fill={true}
+             alt="RoBorregos Logo" 
+             className="bg-black object-contain ..."
+            />
+        </div>
   )
 }
