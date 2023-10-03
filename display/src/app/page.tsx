0acf43750abcd5bf5ef98f9d65e374fb143f08ca
Francisco-SP3 <a01177893@tec.mx>
2023-10-03 16:35:59 -0600

    Logo is now button

--- display/src/app/page.tsx
+++ display/src/app/page.tsx
@@ -1,15 +1,25 @@
+'use client';
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/camera')
+  }
+
   return (
-    <div 
-         className="bg-black ...">
+    <button 
+         className="bg-black ..."
+         onClick={handleClick}>
             <Image 
              src="/images/roborregos_logo.png"
              fill={true}
              alt="RoBorregos Logo" 
              className="bg-black object-contain ..."
             />
-        </div>
+        </button>
   )
 }
