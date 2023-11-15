9ffb6999063038c2b93424466ade3113d91344c0
Francisco-SP3 <A01177893@tec.mx>
2023-11-14 22:25:29 -0600

    Camera functional. Needs topic changes

--- display/src/app/(pages)/camera/page.tsx
+++ display/src/app/(pages)/camera/page.tsx
@@ -1,56 +1,69 @@
-import { GiConsoleController } from "react-icons/gi";
-import { BsCameraFill } from "react-icons/bs";
-import { FaMapLocationDot } from "react-icons/fa6";
-import { HiMiniInformationCircle } from "react-icons/hi2";
-import { GiGrab } from "react-icons/gi";
+"use client"
+import { FiMapPin } from "react-icons/fi";
+import { GiRobotGrab } from "react-icons/gi";
+import { GoPackageDependents } from "react-icons/go";
+import { BsPerson, BsSearch, BsSignStop } from "react-icons/bs";
+import Header from "rbrgs/app/components/header";
 import NavButton from "rbrgs/app/components/NavButton";
+import { useState } from "react";
 
 const CameraPage = () => {
-    return (
-      <div>
-        <nav
-          className="relative flex h-screen w-36 flex-wrap  bg-[#707588] text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600">
-          <div className="flex h-screen flex-wrap items-center justify-between">
-              <button className="bg-[#707588] rounded hover:bg-[#a4abc8] active:bg-[#bec4dd] py-4 px-9 flex-auto">
-                <img
-                  src="/images/roborregos_logo.png"
-                  alt="RoBorregos_Logo"
-                  className="object-scale-down h-16" />
-              </button>
-  
-              <button className="bg-[#707588] rounded hover:bg-[#a4abc8] active:bg-[#bec4dd] py-4 px-10 flex-auto">
-                <GiConsoleController className="text-6xl font-semibold text-neutral-100" href="#"
-                  />
-              </button>
-  
-              <button className="bg-[#a4abc8] rounded hover:bg-[#a4abc8] active:bg-[#bec4dd] py-5 px-11 flex-auto">
-                <BsCameraFill className="text-5xl font-semibold text-neutral-100 dark:text-neutral-200" href="#"
-                  />
-              </button>
-  
-              <button className="bg-[#707588] rounded hover:bg-[#a4abc8] active:bg-[#bec4dd] py-5 px-11 flex-auto">
-                <FaMapLocationDot className="text-5xl font-semibold text-neutral-100 dark:text-neutral-200" href="#"
-                  />
-              </button>
-  
-              <button className="bg-[#707588] rounded hover:bg-[#a4abc8] active:bg-[#bec4dd] py-5 px-11 flex-auto">
-                <GiGrab className="text-5xl font-semibold text-neutral-100 dark:text-neutral-200" href="#"
-                  />
-              </button>
-  
-              <NavButton route={""} />
-            
+
+  const [topic, setTopic] = useState<string>("/zed2/zed_node/confidence/confidence_map");
+
+  return (
+    <div className="p-8 h-full">
+      <div className="flex flex-row">
+        <div>
+          <Header title="Camera" />
+          <p className="mt-2 text-neutral-500 text-lg mb-6">
+                  Select camera vision
+          </p>
+        </div>
+        <div className="flex flex-row ml-96 absolute">
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 rounded-l active:bg-[#bec4dd]"
+              onClick={() => setTopic("/zed2/zed_node/rgb/image_rect_color")}
+            >
+              <div>
+                Normal
+              </div>
+              <div>
+                Vision
+              </div>
+            </button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-4 px-8"
+              onClick={() => setTopic("/zed2/zed_node/confidence/confidence_map")}
+            >
+              <div>
+                Human
+              </div>
+              <div>
+                Recognition
+              </div>
+            </button>
+            <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 rounded-r"
+              onClick={() => setTopic("/zed2/zed_node/rgb/image_rect_gray")}
+            >
+              <div>
+                Object
+              </div>
+              <div>
+                Detection
+              </div>
+            </button>
           </div>
-          
-        </nav>
-
-        <nav
-          className="h-screen w-4 bg-[#ffffff]">
-
-        </nav>
 
       </div>
-    )
+      <div className="flex flex-row justify-center items-center">
+        <div className="w-10/12 border border-10-white">
+          <img
+            src={"http://localhost:8080/stream?topic=" + topic}
+            alt="ZED2_Camera"
+            className="w-full h-full" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CameraPage;
