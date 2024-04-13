e5b3e95aa5b9a1eb20750787435922084e2ce9bd
Francisco-SP3 <a01177893@tec.mx>
2024-04-13 17:39:17 -0600

    refactor: change settings layout

--- display/src/app/(pages)/settings/page.tsx
+++ display/src/app/(pages)/settings/page.tsx
@@ -1,14 +1,20 @@
+"use client"
 import Header from "rbrgs/app/components/header";
-import Status from "rbrgs/app/components/info/status";
+import { useState } from "react";
 
-const Info = () => {
+const SettingsPage = () => {
 
-    return (
-        <div className="p-20">
-            <Header title="Info" />
-            <Status />
-        </div>
-    );
+  const [topic, setTopic] = useState<string>("/zed2/zed_node/rgb/image_rect_color");
+
+  return (
+    <div className="p-8 h-screen">
+        <Header title="Settings" />
+        <p className="mt-2 text-neutral-500 text-lg mb-6">
+            Edit the Display settings
+        </p>
+        
+    </div>
+  )
 }
 
-export default Info;
+export default SettingsPage;
