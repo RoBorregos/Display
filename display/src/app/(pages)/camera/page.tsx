389cec3ee0878c2094784f17f7876538e7496c8b
Francisco-SP3 <a01177893@tec.mx>
2024-04-14 15:51:58 -0600

    feat(visualize): added toggle buttons to change through webviz layouts

--- display/src/app/(pages)/camera/page.tsx
+++ display/src/app/(pages)/camera/page.tsx
@@ -4,58 +4,28 @@ import { useState } from "react";
 
 const CameraPage = () => {
 
-  const [topic, setTopic] = useState<string>("/zed2/zed_node/rgb/image_rect_color");
+  const [taskLayout, setTaskLayout] = useState<string>("layoutCam");
 
   return (
-    <div className="p-8 h-screen">
-        <Header title="Vision" />
-        <p className="mt-2 text-neutral-500 text-lg mb-6">
-            Watch what FRIDA &apos s currently seeing
-        </p>
-        <iframe className="w-full h-5/6" src="https://webviz.io/app/?rosbridge-websocket-url=ws://192.168.31.23:9090&layout-url=https://raw.githubusercontent.com/RoBorregos/Display/main/display/public/layouts/layoutCam.json"></iframe>
-    </div>
-  )
-}
-
-export default CameraPage;
-
-/*
-"use client"
-import Header from "rbrgs/app/components/header";
-import { useState } from "react";
-import { BsCameraFill } from "react-icons/bs";
-import { FaMapLocationDot } from "react-icons/fa6";
-import { GiConsoleController, GiGrab } from "react-icons/gi";
-
-const CameraPage = () => {
-  const [topic, setTopic] = useState("/zed2/zed_node/rgb/image_rect_color");
-
-  return (
-    <div>
-      <div className="p-8 h-full">
+    <div className="h-screen">
+      <div className="p-8 h-1/6">
         <div className="flex flex-row ml-96 absolute">
-          <button className={"hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 rounded-l active:bg-gray-600 " + (topic == "/zed2/zed_node/rgb/image_rect_color" ? "bg-gray-400" : "bg-gray-300")}
-            onClick={() => setTopic("/zed2/zed_node/rgb/image_rect_color")}
+          <button className={"hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 rounded-l active:bg-gray-600 " + (taskLayout == "layoutCam" ? "bg-gray-400" : "bg-gray-300")}
+            onClick={() => setTaskLayout("layoutCam")}
           >
-            <div>
-              Normal
-            </div>
-            <div>
-              Vision
-            </div>
+            <p>
+              Camera
+            </p>
           </button>
-          <button className={"bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 active:bg-gray-600 " + (topic == "/zed2/zed_node/confidence/confidence_map" ? "bg-gray-400" : "bg-gray-300")}
-            onClick={() => setTopic("/zed2/zed_node/confidence/confidence_map")}
+          <button className={"bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 active:bg-gray-600 " + (taskLayout == "layoutNav" ? "bg-gray-400" : "bg-gray-300")}
+            onClick={() => setTaskLayout("layoutNav")}
           >
-            <div>
-              Human
-            </div>
-            <div>
-              Recognition
-            </div>
+            <p>
+              Nav
+            </p>
           </button>
-          <button className={"bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 rounded-r active:bg-gray-600 " + (topic == "/detections_image" ? "bg-gray-400" : "bg-gray-300")}
-            onClick={() => setTopic("/detections_image")}
+          <button className={"bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-4 px-8 rounded-r active:bg-gray-600 " + (taskLayout == "/detections_image" ? "bg-gray-400" : "bg-gray-300")}
+            onClick={() => setTaskLayout("/detections_image")}
           >
             <div>
               Object
@@ -66,25 +36,19 @@ const CameraPage = () => {
           </button>
         </div>
 
-        <Header title="Camera" />
+        <Header title="Visualize" />
         <p className="mt-2 text-neutral-500 text-lg mb-6">
-          Select the command to execute
+          Select what you want to visualize
         </p>
-
       </div>
-      <div className="flex flex-row justify-center items-center">
-        <div className="w-10/12 border border-10-white">
-          <img
-            src={"http://localhost:8080/stream?topic=" + topic}
-            alt={topic}
-            className="w-full h-full" />
-        </div>
+      <div className="w-full h-5/6">
+        <iframe className="w-full h-full" src={"https://webviz.io/app/?rosbridge-websocket-url=ws://192.168.31.23:9090&layout-url=https://raw.githubusercontent.com/RoBorregos/Display/main/display/public/layouts/" + taskLayout + ".json"}></iframe>
       </div>
     </div>
 
 
   )
+  
 }
 
 export default CameraPage;
-*/
