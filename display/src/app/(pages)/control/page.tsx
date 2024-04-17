959c16b50444a972ce4c6081d3e046e295c8efb4
Francisco-SP3 <a01177893@tec.mx>
2024-04-17 03:36:08 -0600

    feat: add roborregos favicon, improve look of control buttons

--- display/src/app/(pages)/control/page.tsx
+++ display/src/app/(pages)/control/page.tsx
@@ -20,24 +20,32 @@ const ControlPage = () => {
             <p className="mt-2 text-neutral-500 text-lg mb-6">
                 Choose type of task to execute
             </p>
-            <div className="flex">
-                <button onClick={() => handleClick("tasks")} className={"h-80 rounded bg-[#707588] hover:bg-[#a4abc8] active:bg-[#bec4dd] mx-5 mt-10 flex-auto"}>
-                    <FaHome className="" />
-                    <h1>
-                        Specific @Home Tasks
-                    </h1>
-                    <p>
-                        Tell FRIDA a particular task to execute
-                    </p>
+            <div className="grid grid-cols-2 gap-16 mr-16">
+                <button onClick={() => handleClick("tasks")}>
+                    <div className={"bg-neutral-800 hover:bg-neutral-700 my-4 mr-9 mx-8 p-2 rounded-xl ring-1 ring-offset-0 ring-neutral-500 flex flex-col justify-center h-full w-full"}>
+                        <div className="bg-neutral-900 p-2 w-fit rounded-lg self-center mb-3">
+                            <FaHome className="text-emerald-400 size-60" />
+                        </div>
+                        <h1 className="font-mono text-4xl text-gray-100">
+                            Specific @Home Tasks
+                        </h1>
+                        <p className="text-neutral-400 text-xl">
+                            Tell FRIDA a particular task to execute
+                        </p>
+                    </div>
                 </button>
-                <button onClick={() => handleClick("commands")} className={"h-80 rounded bg-[#707588] hover:bg-[#a4abc8] active:bg-[#bec4dd] mx-5 mt-10 flex-auto"}>
-                    <FaArrowsAlt />
-                    <h1>
-                        General Purpose Commands
-                    </h1>
-                    <p>
-                        Tell FRIDA what specific commands to follow
-                    </p>
+                <button onClick={() => handleClick("commands")}>
+                    <div className={"bg-neutral-800 hover:bg-neutral-700 my-4 mx-8 p-2 rounded-xl ring-1 ring-offset-0 ring-neutral-500 flex flex-col justify-center h-full w-full"}>
+                        <div className="bg-neutral-900 p-2 w-fit rounded-lg self-center mb-3">
+                            <FaArrowsAlt className="text-emerald-400 size-60" />
+                        </div>
+                        <h1 className="font-mono text-4xl text-gray-100">
+                            {"General Purpose \n Commands"}
+                        </h1>
+                        <p className="text-neutral-400 text-xl">
+                            Tell FRIDA what specific commands to follow
+                        </p>
+                    </div>
                 </button>
             </div>
         </div>
