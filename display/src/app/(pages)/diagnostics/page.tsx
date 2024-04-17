d77bfb9a018aaa19f737c89115329623a149d77b
Francisco-SP3 <a01177893@tec.mx>
2024-04-17 03:45:10 -0600

    refactor: add settings layout to page

--- display/src/app/(pages)/diagnostics/page.tsx
+++ display/src/app/(pages)/diagnostics/page.tsx
@@ -6,16 +6,16 @@ import { useState } from "react";
 const DiagnosticsPage = () => {
 
   return (
-    <div className="p-8 h-screen">
+    <div className="h-screen">
+      <div className="p-8 h-1/6">
         <Header title="Diagnostics" />
         <p className="mt-2 text-neutral-500 text-lg mb-6">
             See what the current hardware status is
         </p>
-        <div>
-          {/* <MenuButton/> */}
-        </div> 
-        
-
+      </div>
+      <div className="w-full h-5/6">
+        <iframe className="w-full h-full" src={"https://webviz.io/app/?rosbridge-websocket-url=ws://192.168.31.23:9090&layout-url=https://raw.githubusercontent.com/RoBorregos/Display/main/display/public/layouts/layoutDiagnostics.json"}></iframe>
+      </div>
     </div>
   )
 }
