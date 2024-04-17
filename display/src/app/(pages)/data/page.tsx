2ec01ece5571f8cc416e41fc86e5bb9d74a763ec
Francisco-SP3 <a01177893@tec.mx>
2024-04-17 03:48:03 -0600

    refactor: change settings and data pages

--- display/src/app/(pages)/data/page.tsx
+++ display/src/app/(pages)/data/page.tsx
@@ -7,15 +7,13 @@ const DataPage = () => {
 
   return (
     <div className="p-8 h-screen">
-        <Header title="DataBase" />
-        <p className="mt-2 text-neutral-500 text-lg mb-6">
-            See what information has been stored
-        </p>
-        <div>
-          {/* <MenuButton/> */}
-        </div> 
+      <Header title="DataBase" />
+      <p className="mt-2 text-neutral-500 text-lg mb-6">
+          See what information has been stored
+      </p>
+      <div>
         
-
+      </div> 
     </div>
   )
 }
